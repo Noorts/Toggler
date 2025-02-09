9e68c844f04505a4c93e5de6a1aba7885dd86783
Noorts <50550612+Noorts@users.noreply.github.com>
2025-02-09 23:07:20 +0100

    refactor: move `findReplacementWord` to `TogglesConfig`

--- src/main/java/core/ToggleAction.java
+++ src/main/java/core/ToggleAction.java
@@ -18,6 +18,7 @@ import java.util.regex.Pattern;
 public class ToggleAction extends AnAction {
     private Editor editor;
     private Document document;
+    private SettingsState settingsState;
 
     // Default is to toggle to the next word/symbol in the toggle sequence.
     private boolean toggleForward = true;
@@ -62,9 +63,9 @@ public class ToggleAction extends AnAction {
         this.document = this.editor.getDocument();
         final CaretModel caretModel = this.editor.getCaretModel();
 
-        SettingsState appSettingsState = AppSettings.getInstance().getState();
-        this.regexPatternOfToggles = createRegexPatternOfToggles(appSettingsState.toggles.getToggles());
-        this.partialMatchingIsEnabled = appSettingsState.isPartialMatchingEnabled();
+        this.settingsState = AppSettings.getInstance().getState();
+        this.regexPatternOfToggles = createRegexPatternOfToggles(this.settingsState.toggles.getToggles());
+        this.partialMatchingIsEnabled = this.settingsState.isPartialMatchingEnabled();
 
         /* Bandage (temporary fix) that might help remove the "ghost" caret that
         appears on load of the IDE. */
@@ -146,7 +147,7 @@ public class ToggleAction extends AnAction {
         // If a match was found then toggle it, else display a notification.
         if (!positionOfMatch.isEmpty()) {
             String match = selectedToggleFromCaret.substring(positionOfMatch.get(0), positionOfMatch.get(1));
-            String replacementToggle = findReplacementWord(match, this.toggleForward);
+            String replacementToggle = settingsState.toggles.findReplacementWord(match, this.toggleForward);
 
             /* The replacementToggle should never be null in this case, because
              * if no match was found then the positionOfMatch would be null.
@@ -182,46 +183,6 @@ public class ToggleAction extends AnAction {
                 caret.setSelection(oldPosition, oldPosition);
             }
         }
-    }
-
-    /**
-     * Find the next or previous word/symbol for the provided word/symbol in the
-     * toggles. The provided word/symbol is searched for in the toggles
-     * configured in the plugin settings and the next or previous one in the
-     * sequence is returned. Whether the next or previous toggle in the sequence
-     * is returned depends on the toggleForward parameter.
-     *
-     * @param word          The word/symbol to be replaced.
-     * @param toggleForward Determines whether the next or previous toggle in the sequence is returned.
-     * @return The next/previous word/symbol in the sequence that the provided
-     * word/symbol is part of. Null is returned if the provided word couldn't be
-     * found in the config.
-     */
-    private String findReplacementWord(String word, boolean toggleForward) {
-        SettingsState appSettingsState = AppSettings.getInstance().getState();
-        List<List<String>> toggleWordsStructure = appSettingsState.toggles.getToggles();
-
-        String wordInLowerCase = word.toLowerCase();
-
-        /* O(n) search for the word/symbol to replace. */
-        for (int i = 0; i < toggleWordsStructure.size(); i++) {
-            for (int j = 0; j < toggleWordsStructure.get(i).size(); j++) {
-                if (toggleWordsStructure.get(i).get(j).toLowerCase().equals(wordInLowerCase)) {
-                    /* The next word/symbol in the sequence is retrieved. The
-                       modulo is used to wrap around if the end of the sequence
-                       is reached. */
-                    int sequenceSize = toggleWordsStructure.get(i).size();
-                    return toggleWordsStructure.get(i).get(
-                        toggleForward
-                            ? (j + 1) % sequenceSize // Next
-                            : (j - 1 + sequenceSize) % sequenceSize // Previous
-                    );
-                }
-            }
-        }
-
-        /* The word/symbol could not be found. */
-        return null;
     }
 
     /**
--- src/main/java/core/TogglesConfig.java
+++ src/main/java/core/TogglesConfig.java
@@ -24,6 +24,43 @@ public class TogglesConfig {
         }
     }
 
+    /**
+     * Find the next or previous word/symbol for the provided word/symbol in the
+     * toggles. The provided word/symbol is searched for in the toggles
+     * configured in the plugin settings and the next or previous one in the
+     * sequence is returned. Whether the next or previous toggle in the sequence
+     * is returned depends on the toggleForward parameter.
+     *
+     * @param word          The word/symbol to be replaced.
+     * @param toggleForward Determines whether the next or previous toggle in the sequence is returned.
+     * @return The next/previous word/symbol in the sequence that the provided
+     * word/symbol is part of. Null is returned if the provided word couldn't be
+     * found in the config.
+     */
+    public String findReplacementWord(String word, boolean toggleForward) {
+        String wordInLowerCase = word.toLowerCase();
+
+        /* O(n) search for the word/symbol to replace. */
+        for (int i = 0; i < toggles.size(); i++) {
+            for (int j = 0; j < toggles.get(i).size(); j++) {
+                if (toggles.get(i).get(j).toLowerCase().equals(wordInLowerCase)) {
+                    /* The next word/symbol in the sequence is retrieved. The
+                       modulo is used to wrap around if the end of the sequence
+                       is reached. */
+                    int sequenceSize = toggles.get(i).size();
+                    return toggles.get(i).get(
+                        toggleForward
+                            ? (j + 1) % sequenceSize // Next
+                            : (j - 1 + sequenceSize) % sequenceSize // Previous
+                    );
+                }
+            }
+        }
+
+        /* The word/symbol could not be found. */
+        return null;
+    }
+
     public static TogglesConfig deserialize(@NotNull String togglesString) {
         TogglesConfig config = new TogglesConfig();
         try {
