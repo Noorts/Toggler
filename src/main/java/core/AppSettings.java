b59928fe6bf4819701ed654e57e8e8da7d47bd28
Noorts <50550612+Noorts@users.noreply.github.com>
2025-02-09 21:34:53 +0100

    docs: document location of the persistent plugin configuration file

--- src/main/java/core/AppSettings.java
+++ src/main/java/core/AppSettings.java
@@ -7,6 +7,12 @@ import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import org.jetbrains.annotations.NotNull;
 
+// For the production config navigate to the configuration directory:
+// https://www.jetbrains.com/help/idea/directories-used-by-the-ide-to-store-settings-caches-plugins-and-logs.html#config-directory
+// and then to ./options/togglerPluginSettings.xml.
+
+// The development config used by `runIde` task is stored in the current working directory:
+// ./build/idea-sandbox/config/options/togglerPluginSettings.xml
 @Service
 @State(
     name = "TogglerSettingsState",
