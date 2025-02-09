cac2df0f9137ddb07bc5bc56bc7a390101b75731
Noorts <50550612+Noorts@users.noreply.github.com>
2025-02-09 21:16:35 +0100

    fix: initialize SettingsState
    Fixes bug introduced by commit #0cc48db.

--- src/main/java/core/AppSettingsState.java
+++ src/main/java/core/AppSettingsState.java
@@ -14,6 +14,7 @@ import org.jetbrains.annotations.NotNull;
 )
 public final class AppSettingsState implements PersistentStateComponent<SettingsState> {
     AppSettingsState() {
+        settingsState = new SettingsState();
     }
 
     private SettingsState settingsState;
